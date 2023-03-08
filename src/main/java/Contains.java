e014c4c1f7aa6c9cfc4207d87ad8fa4a92ac80c6
gcruzBwise <gcruz28@wgu.edu>
2023-03-08 22:28:36 +0000

    Complete

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,11 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
-        return false;
+        for (int newArr : arr) {
+            if (newArr == target) {
+               return true;
+            }
+         }
+         return false;
+      }
     }
-}
